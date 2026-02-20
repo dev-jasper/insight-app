cefadc94c174e184930cf2e211a67f137a437e91
qarbontech-jasper <jfontanilla@qarbontech.io>
2026-02-20 18:48:27 +0800

    fix comments

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 
-    # ✅ JWT endpoints
+    # JWT endpoints
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
